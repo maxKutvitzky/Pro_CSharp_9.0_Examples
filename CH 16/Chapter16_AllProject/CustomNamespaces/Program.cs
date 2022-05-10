7832fec9b1b64de9fca949741e94305ac352d717
MaksimKutvitzky <baznalt@gmail.com>
2022-05-11 01:51:42 +0300

    CH 16

---
+++ CH 16/Chapter16_AllProject/CustomNamespaces/Program.cs
@@ -0,0 +1,16 @@
+﻿// Bring in a namespace from the base class libraries.
+using System;
+// Make use of types defined the MyShapes namespace.
+using MyShapes;
+using CustomNamespaces.My3DShapes;
+
+namespace CustomNamespaces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
