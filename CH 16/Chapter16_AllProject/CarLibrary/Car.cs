5d29ada694829623c49367063d69b14af988239b
MaksimKutvitzky <baznalt@gmail.com>
2022-05-12 14:51:22 +0300

    CH 16

--- CH 16/Chapter16_AllProject/CarLibrary/Car.cs
+++ CH 16/Chapter16_AllProject/CarLibrary/Car.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("CSharpCarClient")]
 namespace CarLibrary
 {
     // The abstract base class in the hierarchy.
