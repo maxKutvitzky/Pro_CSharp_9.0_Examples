f3fc1b1fec006d73ccca1162315270e0ab038df8
MaksimKutvitzky <baznalt@gmail.com>
2022-05-19 16:10:33 +0300

    CH 18

---
+++ CH 18/Chapter18_AllProject/DynamicKeyword/VeryDynamicClass.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword
+{
+    internal class VeryDynamicClass
+    {
+        // A dynamic field.
+        private static dynamic _myDynamicField;
+        // A dynamic property.
+        public dynamic DynamicProperty { get; set; }
+        // A dynamic return type and a dynamic parameter type.
+        public dynamic DynamicMethod(dynamic dynamicParam)
+        {
+            // A dynamic local variable.
+            dynamic dynamicLocalVar = "Local variable";
+            int myInt = 10;
+            if (dynamicParam is int)
+            {
+                return dynamicLocalVar;
+            }
+            else
+            {
+                return myInt;
+            }
+        }
+    }
+}
---
+++ CH 18/Chapter18_AllProject/LateBindingWithDynamic/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.CSharp.RuntimeBinder;
+
+namespace LateBindingWithDynamic
+{
+    internal class Program
+    {
+        static void AddWithReflection()
+        {
+            Assembly asm = Assembly.LoadFrom("MathLibrary");
+            try
+            {// Get metadata for the SimpleMath type.
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+                // Create a SimpleMath on the fly.
+                object obj = Activator.CreateInstance(math);
+                // Get info for Add.
+                MethodInfo mi = math.GetMethod("Add");
+                // Invoke method (with parameters).
+                object[] args = { 10, 70 };
+                Console.WriteLine("Result is: {0}", mi.Invoke(obj, args));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void AddWithDynamic()
+        {
+            Assembly asm = Assembly.LoadFrom("MathLibrary");
+
+            try
+            {
+                // Get metadata for the SimpleMath type.
+                Type math = asm.GetType("MathLibrary.SimpleMath");
+
+                // Create a SimpleMath on the fly.
+                dynamic obj = Activator.CreateInstance(math);
+                Console.WriteLine("Result is: {0}", obj.Add(10, 70));
+            }
+            catch (RuntimeBinderException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+        static void Main(string[] args)
+        {
+            AddWithReflection();
+            AddWithDynamic();
+        }
+    }
+}
