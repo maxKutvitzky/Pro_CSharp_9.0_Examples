56f7485cde373a3f3771bf430679d127e4715e8d
MaksimKutvitzky <baznalt@gmail.com>
2022-05-29 12:03:22 +0300

    CH 21

---
+++ CH 21/Chapter21_AllProject/MyConnectionFactory/DataProviderEnum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyConnectionFactory
+{
+    //OleDb is Windows only and is not supported in .NET Core
+    enum DataProviderEnum
+    {
+        SqlServer,
+        #if PC
+         OleDb,
+        #endif
+        Odbc,
+        None
+    }
+}
