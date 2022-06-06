8a3ae320239a9616628c3443234fbfeb8594a9c7
MaksimKutvitzky <baznalt@gmail.com>
2022-06-06 23:50:22 +0300

    CH 21

---
+++ CH 21/Chapter21_AllProject/AutoLot.DataReader/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+Console.WriteLine("***** Fun with Data Readers *****\n");
+// Create and open a connection.
+using (SqlConnection connection = new SqlConnection())
+{
+    var connectionStringBuilder = new SqlConnectionStringBuilder
+    {
+        InitialCatalog = "AutoLot",
+        DataSource = "DESKTOP-H7TV7FI",
+        TrustServerCertificate = true,
+        IntegratedSecurity = true,
+        ConnectTimeout = 30
+    };
+    connection.ConnectionString =connectionStringBuilder.ConnectionString;
+    //connection.ConnectionString =@"Server=DESKTOP-H7TV7FI;DataBase=AutoLot;TrustServerCertificate=True;Trusted_Connection=true";
+    connection.Open();
+    // Create a SQL command object.
+    string sql =
+    @"Select i.id, m.Name as Make, i.Color, i.Petname
+    FROM Inventory i
+    INNER JOIN Makes m on m.Id = i.MakeId";
+    SqlCommand myCommand = new SqlCommand(sql, connection);
+    // Obtain a data reader a la ExecuteReader().
+    using (SqlDataReader myDataReader = myCommand.ExecuteReader())
+    {
+        // Loop over the results.
+        while (myDataReader.Read())
+        {
+            Console.WriteLine($"-> Make: {myDataReader["Make"]}, PetName: {myDataReader["PetName"]}, Color: {myDataReader["Color"]}.");
+        }
+    }
+    ShowConnectionStatus(connection);
+}
+Console.ReadLine();
+
+static void ShowConnectionStatus(SqlConnection connection)
+{
+    // Show various stats about current connection object.
+    Console.WriteLine("***** Info about your connection *****");
+    Console.WriteLine($@"Database location: {connection.DataSource}");
+    Console.WriteLine($"Database name: {connection.Database}");
+    Console.WriteLine($@"Timeout: {connection.ConnectionTimeout}");
+    Console.WriteLine($"Connection state: { connection.State}\n");
+}
---
+++ CH 21/Chapter21_AllProject/Test/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("config.json", true, true)
+    .Build();
+//Console.WriteLine(config.GetSection("ConnectionStrings")["DefaultConnection"]);
+using (SqlConnection connection = new SqlConnection())
+{
+    SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder
+    {
+        DataSource = "DESKTOP-H7TV7FI",
+        InitialCatalog = "AutoLot",
+        TrustServerCertificate = true,
+        IntegratedSecurity = true
+    };
+    connection.ConnectionString = config["connection"];
+    string sqlCommandText = "SELECT * FROM Inventory";
+    SqlCommand command = new SqlCommand(sqlCommandText, connection);
+    connection.Open();
+    using(SqlDataReader reader = command.ExecuteReader())
+    {
+        while (reader.Read())
+        {
+            Console.WriteLine($"-> Make: {reader["MakeId"]}, PetName: {reader["PetName"]}, Color: {reader["Color"]}.");
+        }
+    }
+    connection.Close();
+}
