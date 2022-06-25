7f8bf568b7137bde8bbaa9e947e7626dcf5aba11
MaksimKutvitzky <baznalt@gmail.com>
2022-06-25 22:52:08 +0300

    CH 24

---
+++ CH 24/Chapter24_AllProject/WpfTesterApp/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTesterApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Closed += MainWindow_Closed;
+            this.Closing += MainWindow_Closing;
+            this.MouseMove += MainWindow_MouseMove;
+            this.KeyDown += MainWindow0s_KeyDown;
+            InitializeComponent();
+        }
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            MessageBox.Show("See ya!");
+        }
+        private void MainWindow0s_KeyDown(object sender, System.Windows.Input.KeyEventArgs e)
+        {
+            // Display key press on the button.
+            ClickMe.Content = e.Key.ToString();
+        }
+        private void MainWindow_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            // Set the title of the window to the current (x,y) of the mouse.
+            this.Title = e.GetPosition(this).ToString();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked the button!");
+        }
+
+        private void Window_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Title = e.GetPosition(this).ToString();
+        }
+
+        private void MyCalendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // See if the user really wants to shut down this window.
+            string msg = "Do you want to close without saving?";
+            MessageBoxResult result = MessageBox.Show(msg,
+            "My App", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (result == MessageBoxResult.No)
+            {
+                // If user doesn't want to close, cancel closure.
+                e.Cancel = true;
+            }
+        }
+    }
+}
