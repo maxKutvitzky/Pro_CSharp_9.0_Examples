37f9e1c6fd778a64b1b455ae65cd9b6e8f390e73
MaksimKutvitzky <baznalt@gmail.com>
2022-06-28 21:05:54 +0300

    CH 25

---
+++ CH 25/Chapter25_AllProject/WpfControlsAndAPIs/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfControlsAndAPIs
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Be in Ink mode by default.
+            this.MyInkCanvas.EditingMode = InkCanvasEditingMode.Ink;
+            this.inkRadio.IsChecked = true;
+            this.comboColors.SelectedIndex = 0;
+        }
+
+        private void RadioButtonClicked(object sender, RoutedEventArgs e)
+        {
+            this.MyInkCanvas.EditingMode = (sender as RadioButton)?.Content.ToString() switch
+            {
+                // These strings must be the same as the Content values for each
+                // RadioButton.
+                "Ink Mode!" => InkCanvasEditingMode.Ink,
+                "Erase Mode!" => InkCanvasEditingMode.EraseByStroke,
+                "Select Mode!" => InkCanvasEditingMode.Select,
+                _ => this.MyInkCanvas.EditingMode
+            };
+        }
+
+        private void ColorChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Get the Tag of the selected StackPanel.
+            string colorToUse = (this.comboColors.SelectedItem as StackPanel).Tag.ToString();
+            // Change the color used to render the strokes.
+            this.MyInkCanvas.DefaultDrawingAttributes.Color =
+            (Color)ColorConverter.ConvertFromString(colorToUse);
+        }
+        private void SaveData(object sender, RoutedEventArgs e)
+        {
+            // Save all data on the InkCanvas to a local file.
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Create))
+            {
+                this.MyInkCanvas.Strokes.Save(fs);
+                fs.Close();
+            }
+            MessageBox.Show("Image Saved", "Saved");
+        }
+        private void LoadData(object sender, RoutedEventArgs e)
+        {
+            // Fill StrokeCollection from file.
+            using (FileStream fs = new FileStream("StrokeData.bin", FileMode.Open, FileAccess.Read))
+            {
+                StrokeCollection strokes = new StrokeCollection(fs);
+                this.MyInkCanvas.Strokes = strokes;
+            }            
+        }
+        private void Clear(object sender, RoutedEventArgs e)
+        {
+            // Clear all strokes.
+            this.MyInkCanvas.Strokes.Clear();
+        }
+    }
+}
